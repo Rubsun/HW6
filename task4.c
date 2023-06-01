62305c94a2cb65176b395f129207723d4c5a32ff
Rubsun <mnudga79@gmail.com>
2023-06-01 18:55:10 +0300

    s_add

--- task4.c
+++ task4.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 int sumDigits(int *num){
-    return *num + *num;
+    int sum;
 
+    while (*num > 0)
+    {
+        sum += *num % 10;
+        *num /= 10;
+    }
+    
+    return sum;
 }
 
 int main(){
     int num;
     scanf("%d", &num);
     printf("%d ", sumDigits(&num));
+    return 0;
 }
